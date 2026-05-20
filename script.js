f2a7e28ef2e03866325530a843ca16cf9d9a0df5
antonindasilva <antonin.dasilva69@gmail.com>
2026-05-20 17:11:24 +0200

    vFinal

--- script.js
+++ script.js
@@ -7,10 +7,32 @@ fetch("tudoDeBom.json")
         console.log(data)
         affichePlat(data.plats)
         afficheServices(data.services)
+        afficheHero(data)
         afficheTemoignage(data.temoignages)
         affichePromesse(data.promessesClient)
-        afficheHero(data)
+        //CARROUSELLE
+
+        var swiper = new Swiper(".mySwiper", {
+            effect: "cards",
+            grabCursor: true,
+            cardsEffect: {
+               perSlideRotate:0,
+               perslideOffSet:100,
+               slideShadow:false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            slidePerView:"auto",
+            loop:true,
+        });
+
+
+
+        //afficheBtn(data)
     });
+
+    AOS.init();
 
 
 //FONCTION POUR MES PLATS
@@ -19,22 +41,27 @@ fetch("tudoDeBom.json")
 //parametre: tableau des plats
 //return: rien elle affiche
 
-function affichePlat(tableauPlats){
-    
+function affichePlat(tableauPlats) {
+
     tableauPlats.forEach(plats => {
 
         document.querySelector("#card-container").innerHTML += `
-        <div class= "card flex gap-32 align-center">
-        <img class="w-30 radius" src="${plats.image}" alt="">
+        
+       
+                        
+                    
+         <div class= "card flex gap-32 align-center" data-aos="fade-up"
+     data-aos-duration="1000">
+            <img class="w-30 radius" src="${plats.image}" alt="">
 
             <div class="w-50">
                 <p class="mb-32 font-bungee">${plats.nom}</p>
 
                 <p class="font-comfortaa">${plats.desc}</p>
             </div>
-            </div>`
+        </div>
 
-
+        `
     });
 }
 
@@ -47,11 +74,12 @@ function affichePlat(tableauPlats){
 
 
 
-function afficheServices(tableauServices){
-    
+function afficheServices(tableauServices) {
+
     tableauServices.forEach(services => {
 
-        document.querySelector("#serviceContainer").innerHTML += `<div class="card-service flex column align-center ">
+        document.querySelector("#serviceContainer").innerHTML += `<div data-aos="fade-up"
+     data-aos-duration="1000" class="card-service flex column align-center ">
                 <img class="w-30 radius" src="${services.image}" alt="">
 
                 <div class="w-40 text-center">
@@ -75,37 +103,40 @@ function afficheServices(tableauServices){
 //return: rien elle affiche
 
 
-function afficheTemoignage(tableauTemoignage){
-    
+function afficheTemoignage(tableauTemoignage) {
+
     tableauTemoignage.forEach(temoignages => {
 
-        document.querySelector("#avisContainer").innerHTML += `
-                    
-                <div class="flex column card-avis font-comfortaa w-100">
-                    <p class="mb-16">"${temoignages.commentaire}"</p>
+        document.querySelector("#carContainer").innerHTML += `
+           <div class="swiper-slide">     
+        <div class="flex column card-avis font-comfortaa w-100">
 
-                    <p class="mb-16">Type d'éxperience: ${temoignages.typeExperience}</p>
+            <p class="mb-16">"${temoignages.commentaire}"</p>
 
-                    <p class="mb-16">${afficheNote(temoignages.note)}</p>
+            <p class="mb-16">Type d'éxperience: ${temoignages.typeExperience}</p>
+
+            <p class="mb-16">${afficheNote(temoignages.note)}</p>
 
 
-                    <div class="flex align-center gap-8">
-                        <p <i class="ph ph-user-square font-size-20"></i></p>
-                        <p class="">${temoignages.prenom}</p>
-                    </div>
+            <div class="flex align-center gap-8">
+                <p <i class="ph ph-user-square font-size-20"></i></p>
+                <p class="">${temoignages.prenom}</p>
+            </div>
 
-                </div>
-
+        </div>
+        </div>
+                
        `
     });
 }
+
 
 
 //role: transformer la note en chaine de caractère etoiles
 //parametre: la note
 //return: chaine de caractère étoiles
 
-function afficheNote(note){
+function afficheNote(note) {
 
 
     //j'arrondi a l'entier supp
@@ -114,23 +145,23 @@ function afficheNote(note){
     //changer ma note en nombre d'étoiles sur 5
     let etoiles = "";
 
-    for(let i=1; i<=maNote; i++){
+    for (let i = 1; i <= maNote; i++) {
         etoiles += "★"
     }
 
-    for(let i=note+1;i<=5 ; i++){
+    for (let i = note + 1; i <= 5; i++) {
         etoiles += "✩"
     }
-    
+
     return etoiles
-    
+
 }
 
-function noteEtoile(note){
-    
+function noteEtoile(note) {
+
     //j'arrondi a l'entier supp
     let maNote = Math.ceil(note)
-    return "★".repeat(maNote)+"✩".repeat(5-maNote)
+    return "★".repeat(maNote) + "✩".repeat(5 - maNote)
 }
 
 
@@ -141,14 +172,15 @@ function noteEtoile(note){
 //return: rien elle affiche
 
 
-function affichePromesse(tableauPromessesClient){
-    
+function affichePromesse(tableauPromessesClient) {
+
     tableauPromessesClient.forEach(promessesClient => {
 
 
         document.querySelector("#promesseContainer").innerHTML += `
                     
-            <div class="font-comfortaa w-30 flex justify-center">
+            <div data-aos="fade-up"
+     data-aos-duration="1000" class="font-comfortaa w-30 flex justify-center">
                 <p class="font-size-40 mb-16">${promessesClient.icone}</p>
                 <p>${promessesClient.phrase}</p>
 
@@ -156,7 +188,8 @@ function affichePromesse(tableauPromessesClient){
 
        `
     }
-)}
+    )
+}
 
 
 //FONCTION HERO
@@ -165,44 +198,55 @@ function affichePromesse(tableauPromessesClient){
 //parametre: clés json  
 //return: rien elle affiche
 
-function afficheHero(données){
+function afficheHero(données) {
 
-    document.querySelector("#heroContainer").innerHTML +=`
-    
-    <div class="bg-jaune w-30 container-1200 w-30">
+    document.querySelector("#h1Container").textContent = `
+        ${données.nomCommercial}`
 
-            <h1 class="mb-16 font-bungee text-shadow">${données.nomCommercial}</h1>
+    document.querySelector("#pContainer").textContent = `
+        ${données.texteAccroche}`
 
-            <p class="mb-32 font-comfortaa">${données.texteAccroche}</p>
+    document.querySelector("#btnContainer").textContent = `
+        ${données.texteBouton}`
 
-            <a class="btn1 font-comfortaa" href="">${données.texteBouton}</a>
-
-        </div>`
 
 }
 
 
 
-//Chnagement couleur header
+
+//Bouton qui ouvre une alerte plus de place !
+
+let reserver = document.querySelector("#btnContainer")
+
+reserver.addEventListener("click", function () {
+
+    alert("NOUS SOMMES COMPLET JUSQU'EN 2027 COMME LES GRANDS BUFFETS DE NARBONE.. NOUS NOUS EXCUSONS POUR CELA..")
+})
+
+
+
+//Changement couleur header
 
 //Je recup le header
 let header = document.querySelector("header")
 //Je recup le hero
 let hero = document.querySelector(".bg-hero")
 // j'ecoute le scroll:
-window.addEventListener("scroll",function(){
-    
+window.addEventListener("scroll", function () {
+
 
     //si l'utilisateur a scrollé plus bas que le hero:
-    if(window.scrollY > hero.clientHeight){
+    if (window.scrollY > hero.clientHeight) {
         header.classList.remove("bg-vert")
-    // je donne au header une classe qui change le bg
-    }else{
+        // je donne au header une classe qui change le bg
+    } else {
         header.classList.add("bg-vert")
 
     }
-})   
+})
 
 
-//Bouton qui ouvre une alerte plus de place !
+
+
 
